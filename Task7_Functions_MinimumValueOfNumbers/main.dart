c1afd22269413e1f6368b8d883f314b0a47bbb6b
Mostafa Saad <83826642+Mostafa11412@users.noreply.github.com>
2024-12-19 10:10:19 +0200

    update on task 7

--- Task7_Functions_MinimumValueOfNumbers/main.dart
+++ Task7_Functions_MinimumValueOfNumbers/main.dart
@@ -2,7 +2,7 @@ void main() {
   List<double> numbers = [5, 1, -55, 123.5, -14.7, 1233, -4, 9];
 
   double result = findMin(numbers: numbers);
-  print('\n' "Minimum Number of List is $result");
+  print('\n' "Minimum Number of List is ${result.toStringAsFixed(2)}");
 }
 
 double findMin({required List<double> numbers}) {
